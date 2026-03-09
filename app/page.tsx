9d9c9879e174a2a0bfc72795964e01fd5563dd2f
Osama Farag <ofarag115@gmail.com>
2026-03-09 11:15:00 -0400

    renamed title and description and reorganized public files

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ const Home = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <Button>click me</Button>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of the application.</p>
     </div>
   );
 };
