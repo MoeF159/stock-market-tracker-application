53ca9e28a4f980ba503513e04bc6ca293cee5300
Osama Farag <ofarag115@gmail.com>
2026-03-09 10:52:14 -0400

    added a test button and redo of the global.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
-    <div>
+    <div className="flex justify-center items-center h-screen">
+      <Button>click me</Button>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main landing page of the application.</p>
     </div>
