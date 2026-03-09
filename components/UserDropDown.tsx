b1dc747371527af7650b095de7e1f8d7b8cd2c24
Osama Farag <ofarag115@gmail.com>
2026-03-09 12:06:50 -0400

    Created a user dropdown menu component

---
+++ components/UserDropDown.tsx
@@ -0,0 +1,25 @@
+import { 
+    DropdownMenu, 
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuTrigger,
+    DropdownMenuSeparator
+} from "@/components/ui/dropdown-menu";
+
+const UserDropDown = () => {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>open</DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+export default UserDropDown
