6f3c67d0d0cdd818271e8537cf07903c4aa47b32
Osama Farag <ofarag115@gmail.com>
2026-03-09 12:24:58 -0400

    added header and dropdown menu to the root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,14 @@
-const Layout = () => {
+import Header from "@/components/Header";
+
+const Layout = ({children} : {children: React.ReactNode}) => {
     return (
-        <div>Layout</div>
+        <main className="min-h-screen text-gray-400">
+            {/* Header */}
+            <Header />
+            <div className="container py-10">
+                {children}
+            </div>
+        </main>
     )
 }
 export default Layout
