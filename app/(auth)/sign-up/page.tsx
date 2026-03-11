6a623a8d87b1261824eaa245bc64975ba19658dd
Osama Farag <ofarag115@gmail.com>
2026-03-11 13:36:30 -0400

    feat: created a submit button. TODO:need the form for sign-up page

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,48 @@
+'use client';
+import { Button } from "@/components/ui/button";
+import {SubmitHandler, useForm} from "react-hook-form";
+
 const SignUp = () => {
+    
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors , isSubmitting },
+    } = useForm<SignUpFormData>({
+        defaultValues: {
+            fullName: "",
+            email: "",
+            password: "",
+            country: "US",
+            investmentGoals: "Growth",
+            riskTolerance: "Medium",
+            preferredIndustrys: "Technology",
+        },
+        mode: 'onBlur'
+    }, );
+
+    
+    const onSubmit =  async (data : SignUpFormData) => {
+        try{
+            console.log("Form Data:", data);
+        }catch(error){
+            console.error("Error during sign up:", error);
+        }
+    }
+    
     return (
-        <div>
-            sign up page
-        </div>
+        <>
+            <h1 className="form-title"> Sign Up & Personalize</h1>
+
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+                {/* Form fields would go here */}
+
+                <Button type="submit" disabled={isSubmitting} className="yellow-btn w-full mt-5">
+                    {isSubmitting ? "Creating Account..." : "Start Your Investing Journey"}
+                </Button>
+            </form>
+        </>
     )
 }
 
