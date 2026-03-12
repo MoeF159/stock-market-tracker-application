285b8f0a4967c555fffddd5b668d0fd269902099
Osama Farag <ofarag115@gmail.com>
2026-03-12 11:44:21 -0400

    fixed a naming mismatch error

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -2,7 +2,7 @@
 
 import { useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
-import InputField from '@/components/forms/InputField';
+import InputField from '@/components/forms/inputField';
 import FooterLink from '@/components/forms/FooterLink';
 
 
